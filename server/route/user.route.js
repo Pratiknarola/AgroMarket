bbc9b93ae33448a601ca02106dc73917354a7be1
Pratik Narola <narolapratik11@gmail.com>
2021-04-10 09:51:46 +0530

    get profile working

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,38 +1,39 @@
-const { authJwt } = require("../middleware");
+const {authJwt} = require("../middleware");
 const controller = require("../controller/user.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-      );
-      next();
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
     });
-    
+
     app.get("/api/test/all", controller.allAccess);
-    
+
     app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
 
+    app.get("/api/user/:username",[authJwt.verifyToken], controller.getprofile);
     app.get("/api/getpastauction", [authJwt.verifyToken, authJwt.isBuyer], controller.getpastauctions);
     app.get("/api/getpresentauction", [authJwt.verifyToken, authJwt.isBuyer], controller.getpresentauctions);
     app.get("/api/getfutureauction", [authJwt.verifyToken, authJwt.isBuyer], controller.getfutureauctions);
-    
+
     app.get(
-      "/api/test/farmer",
-      [authJwt.verifyToken, authJwt.isFarmer],
-      controller.farmerBoard
-      );
-      
-      app.get(
+        "/api/test/farmer",
+        [authJwt.verifyToken, authJwt.isFarmer],
+        controller.farmerBoard
+    );
+
+    app.get(
         "/api/test/buyer",
         [authJwt.verifyToken, authJwt.isBuyer],
         controller.buyerBoard
-        );
-        
-        app.get(
-          "/api/test/admin",
-          [authJwt.verifyToken, authJwt.isAdmin],
-          controller.adminBoard
-          );
-        };
+    );
+
+    app.get(
+        "/api/test/admin",
+        [authJwt.verifyToken, authJwt.isAdmin],
+        controller.adminBoard
+    );
+};
