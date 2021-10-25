652a621860521b9a88a69a18e1efdf1260b06f3f
ukkin <nikharshah2000@gmail.com>
2021-10-25 11:23:35 +0530

    addition of prediction route and controller

--- server/route/farmer.route.js
+++ server/route/farmer.route.js
@@ -2,14 +2,16 @@ const authJwt = require("../middleware/authJwt");
 const controller = require("../controller/farmer.controller");
 
 module.exports = function (app) {
+  //app.get("/api/auth/confirm/:confirmationCode", controller.verifyUser);
 
-    //app.get("/api/auth/confirm/:confirmationCode", controller.verifyUser);
+  app.post(
+    "/api/farmer/createauction",
+    [authJwt.verifyToken, authJwt.isFarmer],
+    controller.createauction
+  );
 
-    app.post(
-        "/api/farmer/createauction",
-        [authJwt.verifyToken, authJwt.isFarmer],
-        controller.createauction
-    );
-
-    app.get("/api/farmer/getcrop/:username", [[authJwt.verifyToken, authJwt.isFarmer], controller.getcroplist])
+  app.get("/api/farmer/getcrop/:username", [
+    [authJwt.verifyToken, authJwt.isFarmer],
+    controller.getcroplist,
+  ]);
 };
---
+++ server/route/prediction.route.js
@@ -0,0 +1,2 @@
+const authJwt = require("../middleware/authJwt");
+const controller = require("../controller/prediction.controller");
